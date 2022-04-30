a7e73c13f08b00b54129b0090ffd3e8489b54342
Rachel Cunningham <rachel.cunningham787@yahoo.co.uk>
2022-04-30 08:57:07 +0100

    Update 4_Process_and_Store_Data.py

--- 4_Process_and_Store_Data.py
+++ 4_Process_and_Store_Data.py
@@ -20,29 +20,29 @@ class Transformation:
         else:
             print('Unkown Data Source!!! Please try again...')
             
-    # Economy Data Transformation
-    def apiOceanDepth(self):
-        depth = {}
-        for record in self.data['ocean_depth:m']:
-            ocean_depth={}
-            
-            # taking out yearly GDP value from records
-            gdp['GDP_in_rs_cr'] = int(record['gross_domestic_product_in_rs_cr_at_2004_05_prices'])
-            gdp_india[record['financial_year']] = gdp
-            gdp_india_yrs = list(gdp_india)
-            
-        for i in range(len(gdp_india_yrs)):
-            if i == 0:
-                pass
-            else:
-                key = 'GDP_Growth_' + gdp_india_yrs[i]
-                # calculating GDP growth on yearly basis
-                gdp_india[gdp_india_yrs[i]][key] = round(((gdp_india[gdp_india_yrs[i]]['GDP_in_rs_cr'] -gdp_india[gdp_india_yrs[i-1]]['GDP_in_rs_cr'])/gdp_india[gdp_india_yrs[i-1]]['GDP_in_rs_cr'])*100,2)
+    # Ocean Depth Data Transformation
+    def apiPollution(self):
+        air_data = self.data['results']
+        
+        # Converting nested data into linear structure
+        depth_list = []
+        for data in air_data:
+            for measurement in data['measurements']:
+                air_dict = {}
+                air_dict['city'] = data['city']
+                air_dict['country'] = data['country']
+                air_dict['parameter'] = measurement['parameter']
+                air_dict['value'] = measurement['value']
+                air_dict['unit'] = measurement['unit']
+                air_list.append(air_dict)
+                
+        # Convert list of dict into pandas df
+        df = pd.DataFrame(air_list, columns=air_dict.keys())
         
         # connection to mongo db
-        mongoDB_obj = MongoDB(urllib.parse.quote_plus('root'), urllib.parse.quote_plus('password'), 'host', 'GDP')
+        mongoDB_obj = MongoDB(urllib.parse.quote_plus('root'), urllib.parse.quote_plus('password'), 'host', 'Pollution_Data')
         # Insert Data into MongoDB
-        mongoDB_obj.insert_into_db(gdp_india, 'India_GDP')
+        mongoDB_obj.insert_into_db(df, 'Air_Quality_India')
         
     # Pollution Data Transformation
     def apiPollution(self):
