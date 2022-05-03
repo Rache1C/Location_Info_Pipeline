1d2e3f356b5db7a90b3ba3e6538a549a2a434420
Rachel Cunningham <rachel.cunningham787@yahoo.co.uk>
2022-05-03 19:54:03 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -2,7 +2,7 @@ import sql_queries
 import extract
 
 # Extract data from API and send to csv (stored locally)
-# pass latitude and longitude of choice
+# pass latitude and longitude of choice, this should have two decimal places.
 extract.get_gust_data('50.57', '-2.45')
 extract.get_direction_data('50.57', '-2.45')
 extract.create_data_set()
